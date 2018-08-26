59759b801bf08f97d7d3dbb08f16530c6b86e9e1
fishedee <306766045@qq.com>
2018-08-26 19:07:11 +0800

    AutoComplete改为普通的onChange方法

--- src/components/MyAutoComplete/index.js
+++ src/components/MyAutoComplete/index.js
@@ -2,37 +2,20 @@ import React from 'react';
 import {AutoComplete} from 'antd';
 
 export default class MyAutoComplete extends React.Component{
-
 	constructor(props){
 		super(props);
-		this.state = {
-			value:'',
-			prevValue:'',
-		};
-	}
-	onBlur = ()=>{
-		this.props.onChange(this.state.value);
-	}
-	onChange = (data)=>{
-		this.state.value = data;
-		this.setState({});
 	}
 	render = ()=>{
 		let { value,dataSource , onChange,filterOption,...resetProps } = this.props; 
 		filterOption = filterOption || function(inputValue,option){
 			 return option.props.children.toLowerCase().indexOf(inputValue.toLowerCase()) !== -1;
 		}
-		if( value != this.state.prevValue ){
-			this.state.value = value;
-			this.state.prevValue = value;
-		}
 		return (<AutoComplete 
 			backfill={true}
 			dataSource={dataSource} 
 			filterOption={filterOption}
-			onBlur={this.onBlur} 
-			value={this.state.value}
-			onChange={this.onChange}
+			onChange={onChange}
+			value={value}
 			{...resetProps}/>);
 	}
 }
